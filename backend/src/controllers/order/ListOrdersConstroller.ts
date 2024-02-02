469e82bdf5fb8357c81c95620cc49626bc8c0767
André Filipe Teixeira de Lima <andrefilipedelima@hotmail.com>
2024-02-01 22:26:23 -0300

    adicionado rotas de orders

---
+++ backend/src/controllers/order/ListOrdersConstroller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { ListOrdersService } from '../../services/order/ListOrdersService';
+
+class ListOrdersConstroller {
+    async handle(req: Request, res: Response) {
+        
+        const listOrdersService = new ListOrdersService();
+
+        const orders = await listOrdersService.execute();
+
+        return res.json(orders);
+
+    }
+
+}
+
+export { ListOrdersConstroller };
